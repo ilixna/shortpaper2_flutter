3d937a3ccf6689ecd397fedadb849f7ccd53ce36
CRONOXT <59267988+CRONOXT@users.noreply.github.com>
2022-06-13 17:31:06 -0400

    Se actualizo el main
    Se agregó la dependencia “provider” para manejar los cambio de estado de las búsquedas de doctores, en toda la vista.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:shortpaper2_flutter/pantalla/lista_doctores.dart';
+import 'package:shortpaper2_flutter/estado/doctores_estado.dart';
+import 'package:provider/provider.dart';
+import 'package:shortpaper2_flutter/server/repositorio/doctores_request.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_)=> EstadoDoctor(request: DoctoresRequest()))
+        ],
+      child: const MyApp()
+    )
+  );
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
